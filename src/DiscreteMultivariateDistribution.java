4e6101ae42aff18855d8793dd5814deb6feda0d8
Ian Mays <maysi@go.stockton.edu>
2023-12-03 20:20:52 -0500

    Added marginal and conditional functions

--- src/DiscreteMultivariateDistribution.java
+++ src/DiscreteMultivariateDistribution.java
@@ -1,6 +1,7 @@
 /**
  * Contains methods for calculations related to discrete multivariate (specifically
- * bivariate) distributions, particularly the joint probability and joint distribution
+ * bivariate) distributions. These include the joint probability, joint
+ * distribution, marginal probability, and conditional probability/distribution
  * functions.
  * 
  * @author Ian Mays
@@ -9,7 +10,7 @@ public class DiscreteMultivariateDistribution {
 	private StatsExceptionChecker checkFor = new StatsExceptionChecker();
 	
 	/**
-	 * 
+	 * Calculates the joint probability function for a discrete bivariate distribution.
 	 * @param p The bivariate probability function, as an array of arrays of doubles
 	 * @param y1 The value for the first random variable
 	 * @param y2 The value for the second random variable
@@ -25,7 +26,8 @@ public class DiscreteMultivariateDistribution {
 	}
 	
 	/**
-	 * 
+	 * Calculates the joint distribution function for a discrete bivariate
+	 * distribution.
 	 * @param p The bivariate probability function, as an array of arrays of doubles
 	 * @param y1 The value for the first random variable
 	 * @param y2 The value for the second random variable
@@ -43,6 +45,128 @@ public class DiscreteMultivariateDistribution {
 			for (int j = 0; j <= y2; j++) {
 				totalProb += p[i][j];
 			}
+		}
+		
+		return totalProb;
+	}
+	
+	/**
+	 * Calculates the marginal probability for a given y1 value.
+	 * @param p The bivariate probability function, as an array of arrays of doubles
+	 * @param y1 The value for the first random variable
+	 * @return The probability that Y1 = y1
+	 * @throws StatsException if p is not a valid bivariate function, or if y1 is
+	 * out of the bounds of p
+	 */
+	public double marginalProbability1(double[][] p, int y1) {
+		checkFor.invalidMultivariate(p);
+		checkFor.indicesOutOfRange(p, y1, 0);
+		
+		double totalProb = 0;
+		
+		for (int i = 0; i < p[y1].length; i++) {
+			totalProb += p[y1][i];
+		}
+		
+		return totalProb;
+	}
+	
+	/**
+	 * Calculates the marginal probability for a given y2 value.
+	 * @param p The bivariate probability function, as an array of arrays of doubles
+	 * @param y1 The value for the second random variable
+	 * @return The probability that Y2 = y2
+	 * @throws StatsException if p is not a valid bivariate function, or if y2 is
+	 * out of the bounds of p
+	 */
+	public double marginalProbability2(double[][] p, int y2) {
+		checkFor.invalidMultivariate(p);
+		checkFor.indicesOutOfRange(p, 0, y2);
+		
+		double totalProb = 0;
+		
+		for (int i = 0; i < p.length; i++) {
+			totalProb += p[i][y2];
+		}
+		
+		return totalProb;
+	}
+	
+	/**
+	 * Calculates the conditional probability for a given y1 value.
+	 * @param p The bivariate probability function, as an array of arrays of doubles
+	 * @param y1 The value for the first random variable
+	 * @param y2 The value for the second random variable
+	 * @return The probability that Y1 = y1 given that Y2 = y2
+	 * @throws StatsException if p is not a valid bivariate function, if y1 or y2 are
+	 * out of the bounds of p, or if the marginal probability of y2 is 0
+	 */
+	public double conditionalProbability12(double[][] p, int y1, int y2) {
+		checkFor.invalidMultivariate(p);
+		checkFor.indicesOutOfRange(p, y1, y2);
+		checkFor.zero(marginalProbability2(p, y2));
+		
+		return jointProbability(p, y1, y2) / marginalProbability2(p, y2);
+	}
+	
+	/**
+	 * Calculates the conditional probability for a given y2 value.
+	 * @param p The bivariate probability function, as an array of arrays of doubles
+	 * @param y1 The value for the first random variable
+	 * @param y2 The value for the second random variable
+	 * @return The probability that Y2 = y2 given that Y1 = y1
+	 * @throws StatsException if p is not a valid bivariate function, if y1 or y2 are
+	 * out of the bounds of p, or if the marginal probability of y1 is 0
+	 */
+	public double conditionalProbability21(double[][] p, int y1, int y2) {
+		checkFor.invalidMultivariate(p);
+		checkFor.indicesOutOfRange(p, y1, y2);
+		checkFor.zero(marginalProbability1(p, y1));
+		
+		return jointProbability(p, y1, y2) / marginalProbability1(p, y1);
+	}
+	
+	/**
+	 * Calculates the conditional distribution function for a given y1 value.
+	 * @param p The bivariate probability function, as an array of arrays of doubles
+	 * @param y1 The value for the first random variable
+	 * @param y2 The value for the second random variable
+	 * @return The probability that Y1 <= y1 given that Y2 = y2
+	 * @throws StatsException if p is not a valid bivariate function, if y1 or y2 are
+	 * out of the bounds of p, or if the marginal probability of y2 is 0
+	 */
+	public double conditionalDistribution12(double[][] p, int y1, int y2) {
+		checkFor.invalidMultivariate(p);
+		checkFor.indicesOutOfRange(p, y1, y2);
+		checkFor.zero(marginalProbability2(p, y2));
+		
+		double totalProb = 0;
+		
+		for (int i = 0; i <= y1; i++) {
+			totalProb += conditionalProbability12(p, i, y2);
+		}
+		
+		return totalProb;
+	}
+	
+	/**
+	 * Calculates the conditional distribution function for a given y2 value.
+	 * @param p The bivariate probability function, as an array of arrays of doubles
+	 * @param y1 The value for the first random variable
+	 * @param y2 The value for the second random variable
+	 * @return The probability that Y2 <= y2 given that Y1 = y1
+	 * @throws StatsException if p is not a valid bivariate function, if y1 or y2 are
+	 * out of the bounds of p, or if the marginal probability of y1 is 0
+	 */
+	public double conditionalDistribution21(double[][] p, int y1, int y2) {
+		checkFor.invalidMultivariate(p);
+		checkFor.indicesOutOfRange(p, y1, y2);
+		checkFor.zero(marginalProbability1(p, y1));
+		
+		double totalProb = 0;
+		
+		for (int i = 0; i <= y2; i++) {
+			totalProb += conditionalProbability21(p, y1, i);
 		}
 		
 		return totalProb;
--- src/TestDiscreteMultivariateDistribution.java
+++ src/TestDiscreteMultivariateDistribution.java
@@ -1,6 +1,7 @@
 /**
- * Tester for DiscreteMultivariateDistribution. Finds both the joint probability and
- * joint distribution outputs for each index in an array, and prints them as a table.
+ * Tester for DiscreteMultivariateDistribution. Finds both the joint probability,
+ * joint distribution, marginal probability, conditional probability, and conditional
+ * distribution outputs for each index in an array, and prints them as a table.
  * 
  * @author Ian Mays
  */
@@ -8,7 +9,7 @@ public class TestDiscreteMultivariateDistribution {
 	public static void main(String[] args) {
 		DiscreteMultivariateDistribution discreteMultivariate = new DiscreteMultivariateDistribution();
 		
-		double[][] p = {{1.0/9, 2.0/9, 1.0/9}, {2.0/9, 2.0/9, 0}, {1.0/9, 0, 0}};
+		double[][] p = {{1.0/9, 2.0/9, 1.0/9}, {2.0/9, 1.0/9, 0}, {1.0/9, 1.0/9, 0}};
 		
 		System.out.printf("%10s", "p(y1,y2) =");
 		
@@ -42,6 +43,96 @@ public class TestDiscreteMultivariateDistribution {
 			
 			for (int j = 0; j < p[i].length; j++) {
 				System.out.printf("%10f", discreteMultivariate.jointDistribution(p, i, j));
+			}
+			
+			System.out.println();
+		}
+		
+		System.out.println();
+		System.out.printf("%10s%10s\n", "y1", "p1(y1)");
+		
+		for (int i = 0; i < p.length; i++) {
+			System.out.printf("%10d%10f\n", i, discreteMultivariate.marginalProbability1(p, i));
+		}
+		
+		System.out.println();
+		System.out.printf("%10s%10s\n", "y2", "p2(y2)");
+		
+		for (int i = 0; i < p[0].length; i++) {
+			System.out.printf("%10d%10f\n", i, discreteMultivariate.marginalProbability2(p, i));
+		}
+		
+		System.out.println();
+		System.out.printf("%10s", "p(y1|y2) =");
+		
+		for (int i = 0; i < p[0].length; i++) {
+			System.out.printf("%10d", i);
+		}
+		
+		System.out.println();
+		
+		for (int i = 0; i < p.length; i++) {
+			System.out.printf("%10d", i);
+			
+			for (int j = 0; j < p[i].length; j++) {
+				System.out.printf("%10f", discreteMultivariate.conditionalProbability12(p, i, j));
+			}
+			
+			System.out.println();
+		}
+		
+		System.out.println();
+		System.out.printf("%10s", "p(y2|y1) =");
+		
+		for (int i = 0; i < p[0].length; i++) {
+			System.out.printf("%10d", i);
+		}
+		
+		System.out.println();
+		
+		for (int i = 0; i < p.length; i++) {
+			System.out.printf("%10d", i);
+			
+			for (int j = 0; j < p[i].length; j++) {
+				System.out.printf("%10f", discreteMultivariate.conditionalProbability21(p, i, j));
+			}
+			
+			System.out.println();
+		}
+		
+		System.out.println();
+		System.out.printf("%10s", "F(y1|y2) =");
+		
+		for (int i = 0; i < p[0].length; i++) {
+			System.out.printf("%10d", i);
+		}
+		
+		System.out.println();
+		
+		for (int i = 0; i < p.length; i++) {
+			System.out.printf("%10d", i);
+			
+			for (int j = 0; j < p[i].length; j++) {
+				System.out.printf("%10f", discreteMultivariate.conditionalDistribution12(p, i, j));
+			}
+			
+			System.out.println();
+		}
+		
+		System.out.println();
+		System.out.printf("%10s", "F(y2|y1) =");
+		
+		for (int i = 0; i < p[0].length; i++) {
+			System.out.printf("%10d", i);
+		}
+		
+		System.out.println();
+		
+		for (int i = 0; i < p.length; i++) {
+			System.out.printf("%10d", i);
+			
+			for (int j = 0; j < p[i].length; j++) {
+				System.out.printf("%10f", discreteMultivariate.conditionalDistribution21(p, i, j));
 			}
 			
 			System.out.println();
