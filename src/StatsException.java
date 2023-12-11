458e90066f91673bea7f94c3e2499d4148dd122e
Ian Mays <maysi@go.stockton.edu>
2023-12-11 13:11:06 -0500

    Added comments to StatsException

--- src/StatsException.java
+++ src/StatsException.java
@@ -1,4 +1,15 @@
+/**
+ * An exception thrown in various cases throughout this StatsLibrary.
+ * StatsExceptionChecker checks for all of these cases and throws errors when they are
+ * found.
+ * 
+ * @author Ian Mays
+ */
 public class StatsException extends RuntimeException {
+	/**
+	 * Constructs a new StatsException as a RuntimeException.
+	 * @param s The text to show for the exception
+	 */
 	public StatsException(String s) {
 		super(s);
 	}
